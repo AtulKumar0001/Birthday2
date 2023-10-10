56f5ed98aacf6f94e940b4b4ab98c5163b7cb1cd
Atul Kumar <95268815+AtulKumar0001@users.noreply.github.com>
2023-10-10 21:45:44 +0530

    Add files via upload

---
+++ birthday/config.js
@@ -0,0 +1,29 @@
+var config = {
+  
+    texts: [
+        "Happy Birthday",     
+        "PANDA",  
+        "I",
+        "HOPE",
+        "ALL YOUR",
+        "BIRTHDAY WISHES",
+        "AND",
+        "DREAMS",
+        "COME",
+        "TRUE",
+        "✌~~",
+    ],
+    imgs: {
+        "PANDA": "./imgs/panda.jpg",
+    },
+    desc: {
+        turn_on: "Turn On Lights",
+        play: "Play Music",
+        bannar_coming: "Let's Decorate",
+        balloons_flying: "Fly Balloons",
+        cake_fadein: "Most Delicious Cake Ever😌",
+        light_candle: "Light Candle？",
+        wish_message: "Happy Birthday",
+        story: "A MESSAGE FOR YOU",
+    }
+};
